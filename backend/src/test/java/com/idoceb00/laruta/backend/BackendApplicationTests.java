7f04bb5615ee02a41c7a1157bff734bde08f077c
idoceb00 <idoceb00@gmail.com>
2026-09-22 16:50:48 +0200

    Initial commit

---
+++ backend/src/test/java/com/idoceb00/laruta/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idoceb00.laruta.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
