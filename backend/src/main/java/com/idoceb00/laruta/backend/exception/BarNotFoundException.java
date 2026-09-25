9d3db8d87270734a324b6be59266e4433ed13ad0
idoceb00 <idoceb00@gmail.com>
2026-09-25 12:11:09 +0200

    docs: add explanation comment on BarNotFoundException

--- backend/src/main/java/com/idoceb00/laruta/backend/exception/BarNotFoundException.java
+++ backend/src/main/java/com/idoceb00/laruta/backend/exception/BarNotFoundException.java
@@ -1,5 +1,6 @@
 package com.idoceb00.laruta.backend.exception;
 
+// If a search is launched specifically by ID, any other type of search would not constitute an exceptional error
 public class BarNotFoundException extends RuntimeException {
     public BarNotFoundException(String message) {
         super(message);
